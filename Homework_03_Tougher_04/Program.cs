37bc15929c6087f918a4890009b85b563ae2cbe1
Michal Obrycki <bambambku@gmail.com>
2023-10-17 12:35:14 +0100

    Add project files.

---
+++ Homework_03_Tougher_04/Program.cs
@@ -0,0 +1,24 @@
+﻿
+//var months = new Dictionary<string month, int days>();
+var months = new (string month, int daysCount)[]
+{
+    ("January", 31),
+    ("February", 28),
+    ("March", 31),
+    ("April", 30),
+    ("May", 31),
+    ("June", 30),
+    ("July", 31),
+    ("August", 31),
+    ("September", 30),
+    ("October", 31),
+    ("November", 30),
+    ("December", 31)
+};
+
+class Date
+{
+    int _day;
+    int _month;
+    int _year;
+}
