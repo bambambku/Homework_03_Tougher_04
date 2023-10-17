9aff8990d3d8d61bb0983738b15e51c7c0a777bb
Michal Obrycki <bambambku@gmail.com>
2023-10-17 19:10:46 +0100

    static class MonthsArray excluded from Date class

--- Homework_03_Tougher_04/Program.cs
+++ Homework_03_Tougher_04/Program.cs
@@ -1,33 +1,14 @@
 ﻿
 
-using System.Runtime.InteropServices;
-
-
-var months = new (string month, int daysCount)[]
-{
-    ("January", 31),
-    ("February", 28),
-    ("March", 31),
-    ("April", 30),
-    ("May", 31),
-    ("June", 30),
-    ("July", 31),
-    ("August", 31),
-    ("September", 30),
-    ("October", 31),
-    ("November", 30),
-    ("December", 31)
-};
-
 Date trainDate = new Date(15, 6, 1994);
-int daysAdded = 100;
+int daysAdded = 500;
 Console.WriteLine($"After adding {daysAdded} days to the {DateFunctions.SlashFormatted(trainDate)} date, ");
 DateFunctions.AddDays(trainDate, daysAdded);
 Console.WriteLine($"the new date is: {DateFunctions.SlashFormatted(trainDate)}");
 
 Console.WriteLine("\n");
 
-int monthsAdded = 2;
+int monthsAdded = 7;
 Console.WriteLine($"After adding {monthsAdded} months to the {DateFunctions.SlashFormatted(trainDate)} date, ");
 DateFunctions.AddMonths(trainDate, monthsAdded);
 Console.WriteLine($"the new date is: {DateFunctions.SlashFormatted(trainDate)}");
@@ -41,13 +22,15 @@ Console.WriteLine($"the new date is: {DateFunctions.SlashFormatted(trainDate)}")
 
 Console.WriteLine("\n" + DateFunctions.SlashFormatted(trainDate));
 
-//Console.WriteLine("\n" + NicelyFormatted(trainDate));
+static class LeapYear
+{
+    public static bool CheckIfLeap(int year)
+    {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) return true;
+        return false;
+    }
+}
 
-//string NicelyFormatted(Date date)
-//{
-//    string[] endings = { "st", "nd", "rd", "th" };
-//    
-//}
 static class DateFunctions
 {
     public static string SlashFormatted(Date date)
@@ -62,44 +45,53 @@ static class DateFunctions
 
     public static Date AddMonths(Date date, int monthsToAdd)
     {
-        date.Month += monthsToAdd;
+        int monthsSum = date.Month + monthsToAdd;
+        int yearsToAdd = monthsSum / 12;
+        int monthsLeft = monthsSum - 12 * yearsToAdd;
+        date.Month = monthsLeft;
+        date.Year += yearsToAdd;
         return date;
     }
 
     public static Date AddDays(Date date, int daysToAdd)
     {
+        int currentMonth;
         if (daysToAdd <= 0 || date is null)
         {
-            Console.WriteLine("Wrong arguments used. Please try again ;(");
+            Console.WriteLine("Wrong arguments used. Returning input as output. ;(");
+            return date;
         }
-        int currentMonth = date.Month - 1;
-        if (date.Day + daysToAdd <= date.Months[currentMonth].daysCount)
+        currentMonth = date.Month - 1;
+        Console.WriteLine(currentMonth);
+        //int currentMonth = 2;
+        if (date.Day + daysToAdd <= MonthsArray.Months[currentMonth].daysCount)
         {
             date.Day += daysToAdd;
         }
         else
         {
             int daysLeft = daysToAdd;
-            daysLeft -= date.Months[currentMonth].daysCount - date.Day + 1;
+            daysLeft -= MonthsArray.Months[currentMonth].daysCount - date.Day + 1;
             date.Day = 1;
             currentMonth++;
-            while (daysLeft > date.Months[currentMonth].daysCount)
+            if (currentMonth > 11) { currentMonth = 0; }
+            while (daysLeft > MonthsArray.Months[currentMonth].daysCount)
             {
-                daysLeft = daysLeft - date.Months[currentMonth].daysCount;
+                daysLeft = daysLeft - MonthsArray.Months[currentMonth].daysCount;
                 currentMonth++;
+                if (currentMonth > 11) { currentMonth = 0; }
             }
             date.Day += daysLeft;
             date.Month = currentMonth + 1;
+            
         }
         return date;
     }
 }
-class Date
+
+static class MonthsArray
 {
-    public int Day;
-    public int Month;
-    public int Year;
-    public (string month, int daysCount)[] Months =
+    public static(string month, int daysCount)[] Months =
     {
     ("January", 31),
     ("February", 28),
@@ -114,14 +106,20 @@ class Date
     ("November", 30),
     ("December", 31)
 };
+}
 
+class Date
+{
+    public int Day;
+    public int Month;
+    public int Year;
 
     public Date(int day, int month, int year)
         {
-            if ((day < 1 || day > Months[month - 1].daysCount) ||
+            if ((day < 1 || day > MonthsArray.Months[month - 1].daysCount) ||
                 (month < 1 || month > 12) ||
                 (year < 1 || year > 2100)) {
-                Console.WriteLine("Input date is not correct.");
+                
             }
             this.Day = day;
             this.Month = month;
